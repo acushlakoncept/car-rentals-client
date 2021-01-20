9f69b12b7836a8a6d7b26e4ff7efec48363801b8
Uduak Essien <acushla4real@gmail.com>
2021-01-20 03:07:42 +0100

    Add basic folder structures and bootstrap module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './pages/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>

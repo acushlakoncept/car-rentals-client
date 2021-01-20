6f8ecc5dbd4013884ad5304904a56d67aa5b0c11
Uduak Essien <acushla4real@gmail.com>
2021-01-20 05:55:50 +0100

    Add feature section

---
+++ src/components/FeatureSection.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import car from '../imgs/feature-ride.png';
+
+export default function FeatureSection() {
+  return (
+    <section className="about-section feature-section">
+      <Row className="mt-5">
+        <Col sm={6}>
+          <img src={car} className="img-fluid" alt="ride" />
+        </Col>
+        <Col sm={6}>
+          <h4 className="about-section__left-title">
+            Best rides for your trips
+          </h4>
+          <p className="about-section__desc">
+            More than hundreds of rides at your disposal. Only the best and checked with owners.
+            We will find best place for you and conditions for a safe parking.
+          </p>
+          <p>
+            <a href="#register" className="btn btn-danger rounded">
+              Learn more
+            </a>
+          </p>
+        </Col>
+      </Row>
+    </section>
+  );
+}
--- src/pages/App.js
+++ src/pages/App.js
@@ -3,12 +3,14 @@ import { Container } from 'react-bootstrap';
 import '../App.scss';
 import Header from '../components/Header';
 import AboutSection from '../components/AboutSection';
+import FeatureSection from '../components/FeatureSection';
 
 function App() {
   return (
     <Container fluid="sm" className="mobile-width">
       <Header />
       <AboutSection />
+      <FeatureSection />
     </Container>
   );
 }

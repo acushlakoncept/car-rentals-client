70db6662d6b365864d6ee2113bb3eccb2d9543d2
Uduak Essien <acushla4real@gmail.com>
2021-01-20 10:52:07 +0100

    Add call to action component

---
+++ src/components/CallToAction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function CallToAction() {
+  return (
+    <section className="call-action">
+      <h4>Be where you want to be on time</h4>
+      <p>
+        <a href="#register" className="btn btn-danger rounded">
+          Book your ride
+        </a>
+      </p>
+    </section>
+  );
+}

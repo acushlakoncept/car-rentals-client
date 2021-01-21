8f7b277baf0f8763cfbaed721291b1f8b99e6d5c
Uduak Essien <acushla4real@gmail.com>
2021-01-21 22:50:53 +0100

    Add conditional logout link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function Navigation() {
+  // const token = useSelector(state => state);
+  // const token = useSelector(state => state.getIn(['user', 'token']));
+  const token = useSelector(state => state.user.token);
+
   return (
     <Navbar collapseOnSelect expand="sm">
       <Navbar.Brand href="/">Car Rentals</Navbar.Brand>
@@ -10,10 +15,18 @@ export default function Navigation() {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto" />
         <Nav>
-          <Link className="nav-link white" to="/login">SIGN IN</Link>
-          <Link className="nav-link" to="/register">
-            <span className="btn-menu">SIGN UP</span>
-          </Link>
+          {
+            token === undefined || token === 'undefined'
+              ? (
+                <>
+                  <Link className="nav-link white" to="/login">SIGN IN</Link>
+                  <Link className="nav-link" to="/register">
+                    <span className="btn-menu">SIGN UP</span>
+                  </Link>
+                </>
+              )
+              : (<Link className="nav-link white" to="/logout">LOGOUT</Link>)
+          }
         </Nav>
       </Navbar.Collapse>
     </Navbar>

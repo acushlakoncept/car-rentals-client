7041e7551efe4a1fb8af2c2b3bd2ac2970c83269
Uduak Essien <acushla4real@gmail.com>
2021-01-21 05:12:17 +0100

    Add default baseURL and auth header for axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 import Routes from './Routes';
 import './App.scss';
+
+axios.defaults.baseURL = 'http://localhost:3001/api/v1';
+axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`;
 
 ReactDOM.render(
   <React.StrictMode>

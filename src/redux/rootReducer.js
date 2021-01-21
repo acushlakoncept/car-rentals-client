416e57ddf7b6c5cbe85bd8d1e8305dbfeff713e0
Uduak Essien <acushla4real@gmail.com>
2021-01-21 22:54:01 +0100

    Add register reducer to root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import carItemReducer from './carItem/carItemReducer';
 import carReducer from './cars/carReducer';
 import userReducer from './users/userReducer';
+import userRegisterReducer from './users/userRegisterReducer';
 
 const rootReducer = combineReducers({
   cars: carReducer,
   carItem: carItemReducer,
   user: userReducer,
+  signup: userRegisterReducer,
 });
 
 export default rootReducer;

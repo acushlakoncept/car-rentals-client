94e1c64e7d31520c7e9748c60ebcd5562359e178
Uduak Essien <acushla4real@gmail.com>
2021-01-21 06:23:12 +0100

    Add fetchCars request and props to carContainer

--- src/container/CarContainer.js
+++ src/container/CarContainer.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { ScaleLoader } from 'react-spinners';
 import CarItem from '../components/CarItem';
+import { fetchCars } from '../redux/actions';
 
-export default function CarContainer() {
-  return (
+function CarContainer({ carData, getCars }) {
+  useEffect(() => {
+    getCars();
+  }, []);
+
+  // eslint-disable-next-line no-nested-ternary
+  return carData.loading ? (
+    <h2 className="text-center pt-5">
+      <ScaleLoader size={16} color="orange" />
+    </h2>
+  ) : carData.error ? (
+    <h2 className="text-center pt-5">
+      { carData.error }
+    </h2>
+  ) : (
     <Container className="bg-dark">
       <div className="mt-5 d-flex flex-wrap justify-content-center">
-        <CarItem />
-        <CarItem />
-        <CarItem />
-        <CarItem />
-        <CarItem />
-        <CarItem />
-        <CarItem />
-        <CarItem />
+        { carData.cars.map(carInfo => (
+          <CarItem
+            key={carInfo.id}
+            car={carInfo}
+          />
+        )) }
       </div>
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  carData: state.cars,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getCars: () => dispatch(fetchCars()),
+});
+
+
+export default CarContainer;

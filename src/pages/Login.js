a9513bdf8e644d6bffe784b42736ad3d2049bd47
Uduak Essien <acushla4real@gmail.com>
2021-01-21 07:46:18 +0100

    Refactor login page using login container

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import LoginContainer from '../container/LoginContainer';
 
 export default function Login() {
   return (
     <Container className="login-box">
-      <h2 className="about-section__primary">
-        Sign in
-      </h2>
-      <p className="mb-3">
-        Hello there! Sign in and book your ride
-      </p>
-      <Form className="w-50">
-        <Form.Group controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button className="btn-oval pl-4 pr-4" type="submit">
-          Sign in
-        </Button>
-        <Link className="nav-link text-danger" to="/">&larr; Back to home</Link>
-      </Form>
+      <LoginContainer />
     </Container>
   );
 }

73a8e44443aa698626326097c4f4fc705d214b6a
Uduak Essien <acushla4real@gmail.com>
2021-01-21 22:50:06 +0100

    Add routes redirection for logout

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Switch, Route,
+} from 'react-router-dom';
 import App from './pages/App';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -12,6 +14,15 @@ const Routes = () => (
       <Route exact path="/cars" component={Cars} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route
+        exact
+        path="/logout"
+        render={() => {
+          localStorage.setItem('CarRentalsToken', undefined);
+          // return <Redirect to="/login" />;
+          window.location.href = '/login';
+        }}
+      />
     </Switch>
   </BrowserRouter>
 );

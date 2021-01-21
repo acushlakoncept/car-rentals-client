795f876181ce3431d7e29722dfaba97ca1a9ec05
Uduak Essien <acushla4real@gmail.com>
2021-01-21 05:12:46 +0100

    Update routes with cars

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './pages/App';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Cars from './pages/Cars';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
+      <Route exact path="/cars" component={Cars} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
     </Switch>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Navigation() {
   return (
     <Navbar collapseOnSelect expand="sm">
-      <Navbar.Brand href="#home">Car Rentals</Navbar.Brand>
+      <Navbar.Brand href="/">Car Rentals</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto" />

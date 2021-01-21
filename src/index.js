c36d76c1fb239f47afca1c983cb6601421e70e1e
Uduak Essien <acushla4real@gmail.com>
2021-01-21 05:41:32 +0100

    Update index.js with provider and redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Routes from './Routes';
 import './App.scss';
+import store from './redux/store';
 
 axios.defaults.baseURL = 'http://localhost:3001/api/v1';
 axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token')}`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

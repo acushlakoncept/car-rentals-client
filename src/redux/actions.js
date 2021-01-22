a1c86a3a507169d1c48bc753f0ced8332e5b04c0
Uduak Essien <acushla4real@gmail.com>
2021-01-22 10:02:14 +0100

    Update actions with addFavourites and fetchFavourites

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,3 +2,5 @@ export { fetchCarItem } from './carItem/carItemActions';
 export { fetchCars } from './cars/carActions';
 export { loginUser } from './users/userAction';
 export { registerUser } from './users/userRegisterAction';
+export { addFavourites } from './favourites/favouriteAction';
+export { fetchFavourites } from './favourites/getFavActions';

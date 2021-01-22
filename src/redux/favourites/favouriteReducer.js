bbb6b621e1fa32cdd3c164de361536b59185c8c8
Uduak Essien <acushla4real@gmail.com>
2021-01-22 10:03:15 +0100

    Add favouriteReducer

---
+++ src/redux/favourites/favouriteReducer.js
@@ -0,0 +1,35 @@
+import * as fav from './favouriteTypes';
+
+const initialState = {
+  loading: false,
+  message: '',
+  error: '',
+};
+
+const favouriteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case fav.ADD_FAVORITE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case fav.ADD_FAVORITE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        message: action.payload,
+        error: '',
+      };
+    case fav.ADD_FAVORITE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        message: '',
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default favouriteReducer;

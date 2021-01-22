ef92ea9097fcc44231a56b0e1f5287dc75a277f3
Uduak Essien <acushla4real@gmail.com>
2021-01-22 10:02:37 +0100

    Update rootReducer with add and fetch favourite

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,12 +3,16 @@ import carItemReducer from './carItem/carItemReducer';
 import carReducer from './cars/carReducer';
 import userReducer from './users/userReducer';
 import userRegisterReducer from './users/userRegisterReducer';
+import favouriteReducer from './favourites/favouriteReducer';
+import getFavReducer from './favourites/getFavReducer';
 
 const rootReducer = combineReducers({
   cars: carReducer,
   carItem: carItemReducer,
   user: userReducer,
   signup: userRegisterReducer,
+  favourites: favouriteReducer,
+  getFavs: getFavReducer,
 });
 
 export default rootReducer;

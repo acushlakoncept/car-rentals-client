a340a09d8485ddb84084ef4645446a380b728024
Uduak Essien <acushla4real@gmail.com>
2021-01-22 10:56:15 +0100

    Update link in call to action component

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CallToAction() {
   return (
     <section className="call-action">
       <h4>Be where you want to be on time</h4>
       <p>
-        <a href="#register" className="btn btn-oval">
-          Book your ride
-        </a>
+        <Link className="btn btn-oval" to="/register">Book your ride</Link>
       </p>
     </section>
   );

47dd62fe8a5ed7bc6b8ced0df5a08fd54e21c0ce
Uduak Essien <acushla4real@gmail.com>
2021-01-22 10:01:20 +0100

    Add add handleAddFavourite method

--- src/container/CarContainer.js
+++ src/container/CarContainer.js
@@ -1,17 +1,27 @@
 import React, { useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { ScaleLoader } from 'react-spinners';
 import { Redirect } from 'react-router-dom';
 import CarItem from '../components/CarItem';
-import { fetchCars } from '../redux/actions';
+import { fetchCars, addFavourites } from '../redux/actions';
 
 function CarContainer({ carData, getCars }) {
   useEffect(() => {
     getCars();
   }, []);
   const token = useSelector(state => state.user.token);
+  const favState = useSelector(state => state.favourites);
+  const dispatch = useDispatch();
+
+  const handleFavourite = id => {
+    const carInfo = {
+      car_id: id,
+    };
+    dispatch(addFavourites(carInfo));
+    alert(favState.message);
+  };
 
   // eslint-disable-next-line no-nested-ternary
   return carData.loading ? (
@@ -26,10 +36,12 @@ function CarContainer({ carData, getCars }) {
   ) : (
     <Container className="bg-dark">
       <div className="mt-5 d-flex flex-wrap justify-content-center">
+        <h2 className="text-center pt-5 white">{ carData.error }</h2>
         { carData.cars.map(carInfo => (
           <CarItem
             key={carInfo.id}
             car={carInfo}
+            handleFavourite={() => handleFavourite(carInfo.id)}
           />
         )) }
       </div>

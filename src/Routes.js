1712b4d317808413d3b76c78da2c585e6bc9b75f
Uduak Essien <acushla4real@gmail.com>
2021-01-22 09:59:34 +0100

    Update routes with user favourites link

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,14 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Cars from './pages/Cars';
 import CarItemDetail from './pages/CarItemDetail';
+import Favourites from './pages/Favourites';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/cars" component={Cars} />
+      <Route exact path="/favourites" component={Favourites} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/cars/:id" component={CarItemDetail} />

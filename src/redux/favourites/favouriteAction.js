f291e711dba15988624b84621e0d24870920757f
Uduak Essien <acushla4real@gmail.com>
2021-02-03 16:32:11 +0100

    Add remove Favourite Action

--- src/redux/favourites/favouriteAction.js
+++ src/redux/favourites/favouriteAction.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import {
-  ADD_FAVORITE_REQUEST, ADD_FAVORITE_SUCCESS, ADD_FAVORITE_FAILURE,
+  ADD_FAVORITE_REQUEST,
+  ADD_FAVORITE_SUCCESS,
+  ADD_FAVORITE_FAILURE,
+  REMOVE_FAVORITE_SUCCESS,
+  REMOVE_FAVORITE_FAILURE,
+  REMOVE_FAVORITE_REQUEST,
 } from './favouriteTypes';
 
 export const addFavoritesRequest = () => ({
@@ -17,6 +22,20 @@ export const addFavoritesFailure = error => ({
   payload: error,
 });
 
+export const removeFavoritesRequest = () => ({
+  type: REMOVE_FAVORITE_REQUEST,
+});
+
+export const removeFavoritesSuccess = message => ({
+  type: REMOVE_FAVORITE_SUCCESS,
+  payload: message,
+});
+
+export const removeFavoritesFailure = error => ({
+  type: REMOVE_FAVORITE_FAILURE,
+  payload: error,
+});
+
 // eslint-disable-next-line func-names
 export const addFavourites = data => function (dispatch) {
   dispatch(addFavoritesRequest());
@@ -28,5 +47,19 @@ export const addFavourites = data => function (dispatch) {
     })
     .catch(error => {
       dispatch(addFavoritesFailure(error.response.data.error));
+    });
+};
+
+// eslint-disable-next-line func-names
+export const removeFavourites = id => function (dispatch) {
+  dispatch(removeFavoritesRequest());
+  axios
+    .delete(`/favourites/${id}`)
+    .then(response => {
+      const { data } = response;
+      dispatch(removeFavoritesSuccess(data.message));
+    })
+    .catch(error => {
+      dispatch(removeFavoritesFailure(error.response.data.message));
     });
 };

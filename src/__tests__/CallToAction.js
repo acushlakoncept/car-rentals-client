787ad92f7891721eafddeda5b7cd873334bc97aa
Uduak Essien <acushla4real@gmail.com>
2021-02-03 17:00:05 +0100

    Remove unwanted test

--- src/__tests__/CallToAction.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import CallToAction from '../components/CallToAction';
-
-it('renders CallToAction correctly', () => {
-  const tree = renderer.create(<CallToAction />);
-  expect(tree).toMatchSnapshot();
-});
--- src/__tests__/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Footer from '../components/Footer';
-
-it('renders Footer correctly', () => {
-  const tree = renderer.create(<Footer />);
-  expect(tree).toMatchSnapshot();
-});
